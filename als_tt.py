86b9aa32043e3eb4f92c73261659c3aac65f355a
Vivek Bharadwaj <vbharadw@caltech.edu>
2018-04-17 11:30:19 -0700

    ALS algorithm converges, but is somewhat volatile; improvements needed.

--- als_tt.py
+++ als_tt.py
@@ -20,9 +20,6 @@ class als_tt:
         self.dim = len(gridIndices)
         self.approx.append(np.random.rand(1, len(gridIndices[0]), rank))
 
-        # Can fix this to preallocate memory, since we know the tensor dimensions in advance
-        self.reversePartials = [np.identity(1)]
-
         for i in range(self.dim - 2):
             self.approx.append(np.array(np.random.rand(rank, len(gridIndices[i + 1]), rank)))
 
@@ -48,20 +45,21 @@ class als_tt:
         res = np.identity(1)
 
         for i in range(self.dim):
-            res = res * self.approx[i][:, int(point[i]), :]
+            res = np.matmul(res, self.approx[i][:, int(point[i]), :])
 
         return res[0, 0]
 
     def compute_error(self):
-        return self.err_recurse(0, np.zeros(self.dim))
-        #/ np.prod([len(x) for x in self.gridIndices])
+        return self.err_recurse(0, np.zeros(self.dim)) / np.prod([len(x) for x in self.gridIndices])
 
     def err_recurse(self, idx, point):
         err = 0
         if idx == self.dim:
-            gridpt = [self.gridIndices[i][0] for i in range(self.dim)]
-            fval = self.fcn(gridpt)
+            gridpt = [self.gridIndices[i][int(point[i])] for i in range(self.dim)]
+            fval = self.fcn(
+                gridpt)
             err = (fval - self.get(point)) ** 2
+            # print("Point: {}, fval: {}, approx: {}".format(gridpt, fval, self.get(point)))
         else:
             for i in range(len(self.gridIndices[idx])):
                 point[idx] = i
@@ -79,23 +77,47 @@ class als_tt:
         gridpt = [self.gridIndices[i][point[i]] for i in range(self.dim)]
         fval = self.fcn(gridpt)
 
+        self.reversePartials = [np.identity(1)]
         # Precompute matrix products so we don't redo the same calculation over and over
         for i in reversed(range(self.dim)):
             self.reversePartials.append(np.matrix(self.approx[i][:, point[i], :]) * self.reversePartials[-1])
 
+        grads = []
         self.reversePartials.reverse()
+
+        # U = self.approx[0][0, point[0], :]
+        # V = self.approx[1][:, point[1], 0]
+
+        # gradU = self.eta * (-V.T * (fval - np.dot(U, V)))
+        # gradV = self.eta * (-U.T * (fval - np.dot(U, V)))
+
+        # print(self.approx[1])
 
         for i in range(self.dim):
             # Compute the approximation
-            y_approx = (forward_partial * self.reversePartials[i])[0, 0]
+            y_approx = np.matmul(forward_partial, self.reversePartials[i])[0, 0]
 
             # Compute the tensor product for use in the gradient.
             outerprod = np.outer(forward_partial, self.reversePartials[i + 1])
             # ALS / SGD update: the term w/ coefficient self.lda is regularization w/ Froebenius norm
-            self.approx[i][:, point[i], :] -= self.eta * ((fval - y_approx) * np.matrix(outerprod) + (self.lda * np.matrix(self.approx[i][:, point[i], :])))
+            grads.append(- self.eta * ((fval - y_approx) * np.matrix(outerprod)))
 
             # Update the forward partial matrix
-            forward_partial = forward_partial * np.matrix(self.approx[i][:, point[i], :])
+            forward_partial = np.matmul(forward_partial, np.matrix(self.approx[i][:, point[i], :]))
+
+        for i in range(len(grads)):
+            # Debugging purposes only...
+            # print("Computed Gradient: ")
+            # print(grads[i])
+            #if i == 0:
+            #    print(gradU)
+            #else:
+            #    print(gradV)
+            self.approx[i][:, point[i], :] -= grads[i]
+
+        #self.approx[0][0, point[0], :] -= gradU
+        #self.approx[1][:, point[1], 0] -= gradV
+
 
     def build(self, num_iter):
         for i in progressbar.progressbar(range(num_iter)):
@@ -103,25 +125,20 @@ class als_tt:
 
 
 def polytest(params):
-    return 2 * params[0] + 3 * params[1]
+    return 2 * params[0] + 3 * params[1] ** 3 + 2 * params[2] ** 2
 
 grid = [[1.0, 2.0, 3.0, 4.0],
+        [1.0, 2.0, 3.0, 4.0],
         [1.0, 2.0, 3.0, 4.0]]
 
 if __name__ == '__main__':
-    test = als_tt(3, grid, polytest, 0.1, 0.0000)
-    print(test.compute_error())
-    # test.build(30)
+    test = als_tt(3, grid, polytest, 0.0005, 0.0000)
 
-    for i in range(1):
-        test.sgd_update([0, 0])
-    #test.sgd_update([0, 0, 0, 1])
     print(test.compute_error())
+    test.build(500)
+    print(test.compute_error())
+
     print("=============")
-    print(test.approx)
 
-    #print(test.get([0, 0, 0, 0]))
-    #gridpt = [test.gridIndices[i][0] for i in range(test.dim)]
-    #print(test.fcn(gridpt))
 
 
